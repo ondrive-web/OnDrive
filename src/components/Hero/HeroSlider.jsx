c4872d0702d25a5755cf4d39ca24dc76a05d182a
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2025-12-03 15:04:34 +0200

    chrono : change slider

--- src/components/Hero/HeroSlider.jsx
+++ src/components/Hero/HeroSlider.jsx
@@ -5,15 +5,9 @@ import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import styles from './Hero.module.css';
-
-import hero1 from '../../assets/images/hero1.jpg';
-import hero2 from '../../assets/images/hero2.jpg';
-import hero3 from '../../assets/images/hero3.jpg';
-import hero4 from '../../assets/images/hero4.jpg';
+import heroSlides from '../../data/HeroSlides.js';
 
 export default function HeroSlider({ onCTAClick }) {
-  const slides = [hero1, hero2, hero3, hero4];
-
   return (
     <div className={styles.hero_background_slider}>
       <Swiper
@@ -29,7 +23,7 @@ export default function HeroSlider({ onCTAClick }) {
         modules={[Autoplay, Pagination, Navigation]}
         className={styles.mySwiper}
       >
-        {slides.map((src, idx) => (
+        {heroSlides.map((src, idx) => (
           <SwiperSlide key={idx}>
             <div
               className={styles.slide}
---
+++ src/data/HeroSlides.js
@@ -0,0 +1,8 @@
+import hero1 from '../assets/images/hero1.jpg';
+import hero2 from '../assets/images/hero2.jpg';
+import hero3 from '../assets/images/hero3.jpg';
+import hero4 from '../assets/images/hero4.jpg';
+
+const heroSlides = [hero1, hero2, hero3, hero4];
+
+export default heroSlides;

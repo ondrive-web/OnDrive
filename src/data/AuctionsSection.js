809f506365094289b27e324566c7aa7b0fa2d34e
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2025-12-15 14:49:44 +0200

    chrono : create countries & auctions files

---
+++ src/data/AuctionsSection.js
@@ -0,0 +1,23 @@
+export const auctionsData = [
+    { 
+      id: 1, 
+      name: 'Copart', 
+      logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Copart_Logo.svg/1200px-Copart_Logo.svg.png',
+      description: 'Світовий лідер онлайн-аукціонів транспортних засобів. Ідеальне місце для пошуку авто під відновлення.',
+      bestFor: 'значних пошкоджень та низької ціни.'
+    },
+    { 
+      id: 2, 
+      name: 'IAAI', 
+      logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/IAA_Inc._Logo.jpg/800px-IAA_Inc._Logo.jpg', 
+      description: 'Аукціон, орієнтований на легкі пошкодження. Часто зустрічаються авто, які можна запустити та поїхати (Run & Drive).',
+      bestFor: 'тих, хто шукає мінімальні пошкодження.'
+    },
+    { 
+      id: 3, 
+      name: 'Manheim', 
+      logo: 'https://upload.wikimedia.org/wikipedia/commons/2/22/Manheim_logo.svg', 
+      description: 'Закритий аукціон виключно для дилерів. Продаються цілі автомобілі без ДТП, лізингові повернення.',
+      bestFor: 'купівлі цілого, надійного авто.'
+    },
+  ];
---
+++ src/data/CountriesSection.jsx
@@ -0,0 +1,8 @@
+export const countriesData = [
+  { id: 1, name: 'Україна', flag: 'https://flagcdn.com/w160/ua.png' },
+  { id: 2, name: 'Євросоюз', flag: 'https://flagcdn.com/w160/eu.png' },
+  { id: 3, name: 'Китай', flag: 'https://flagcdn.com/w160/cn.png' },
+  { id: 4, name: 'Корея', flag: 'https://flagcdn.com/w160/kr.png' },
+  { id: 5, name: 'Канада', flag: 'https://flagcdn.com/w160/ca.png' },
+  { id: 6, name: 'США', flag: 'https://flagcdn.com/w160/us.png' },
+];

6ca346cf43af1d3ad3847fe75e2626d7ee5e9470
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2026-01-08 15:01:45 +0200

    chrono : change slider format img to webp

--- src/data/HeroSlides.js
+++ src/data/HeroSlides.js
@@ -1,7 +1,7 @@
-import hero1 from '../../public/images/hero-slider/black-bmw-auction-usa.jpg';
-import hero2 from '../../public/images/hero-slider/volkswagen-mustang-auction-usa.jpg';
-import hero3 from '../../public/images/hero-slider/volvo-auction-usa.jpg';
-import hero4 from '../../public/images/hero-slider/white-bmw-auction-usa.jpg';
+import hero1 from '../../public/images/hero-slider/black-bmw-auction-usa.webp';
+import hero2 from '../../public/images/hero-slider/volkswagen-auction-usa.webp';
+import hero3 from '../../public/images/hero-slider/volvo-auction-usa.webp';
+import hero4 from '../../public/images/hero-slider/white-bmw-auction-usa.webp';
 
 const heroSlides = [hero1, hero2, hero3, hero4];
 

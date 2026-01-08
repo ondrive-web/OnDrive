ad9741847744b60749ad6fd2d9fdad98c897fb1e
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2026-01-08 14:46:46 +0200

    chrono : slider fix photos

--- src/data/HeroSlides.js
+++ src/data/HeroSlides.js
@@ -1,7 +1,7 @@
-import hero1 from '../assets/images/hero1.jpg';
-import hero2 from '../assets/images/hero2.jpg';
-import hero3 from '../assets/images/hero3.jpg';
-import hero4 from '../assets/images/hero4.jpg';
+import hero1 from '../../public/images/hero-slider/black-bmw-auction-usa.jpg';
+import hero2 from '../../public/images/hero-slider/volkswagen-mustang-auction-usa.jpg';
+import hero3 from '../../public/images/hero-slider/volvo-auction-usa.jpg';
+import hero4 from '../../public/images/hero-slider/white-bmw-auction-usa.jpg';
 
 const heroSlides = [hero1, hero2, hero3, hero4];
 

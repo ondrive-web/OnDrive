4fb1556acd967c416209e2426a73d6148a1cb371
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2026-01-09 10:50:28 +0200

    fix

--- src/components/Hero/HeroSlider.jsx
+++ src/components/Hero/HeroSlider.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination } from 'swiper/modules';
 import 'swiper/css';
-// import 'swiper/css/pagination';
 import styles from './Hero.module.css';
 import heroSlides from '../../data/HeroSlides.js';
 

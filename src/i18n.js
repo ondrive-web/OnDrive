f44f78c7b183225104892aed49b4bdb097a71722
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2026-01-13 16:42:21 +0200

    i18n cnfg

---
+++ src/i18n.js
@@ -0,0 +1,35 @@
+import i18n from 'i18next';
+
+import { initReactI18next } from 'react-i18next';
+
+// --- ВОТ ЗДЕСЬ МЫ ИХ ИМПОРТИРУЕМ ---
+
+import translationUA from './locales/ua/translation.json';
+
+import translationEN from './locales/en/translation.json';
+
+const resources = {
+  ua: {
+    translation: translationUA, // Передаем содержимое файла
+  },
+
+  en: {
+    translation: translationEN, // Передаем содержимое файла
+  },
+};
+
+i18n
+
+  .use(initReactI18next)
+
+  .init({
+    resources,
+
+    lng: 'ua', // Язык по умолчанию
+
+    fallbackLng: 'ua', // Если перевод не найден, показать этот язык
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });

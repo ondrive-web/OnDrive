8b332867c216e81dcc0f9e20c341ce076e6e26e3
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2026-01-21 12:21:09 +0200

    chrono : fix icon color advantages

--- src/data/AdvantagesIcons.jsx
+++ src/data/AdvantagesIcons.jsx
@@ -8,10 +8,10 @@ import {
 } from 'lucide-react';
 
 export const advantagesIcons = [
-  <MessageCircle size={120} color="white" strokeWidth={1} />,
-  <Search size={120} color="white" strokeWidth={1} />,
-  <Zap size={120} color="white" strokeWidth={1} />,
-  <ClipboardCheck size={120} color="white" strokeWidth={1} />,
-  <Wrench size={120} color="white" strokeWidth={1} />,
-  <Headset size={120} color="white" strokeWidth={1} />,
+  <MessageCircle size={120} color="#fd7902" strokeWidth={1} />,
+  <Search size={120} color="#fd7902" strokeWidth={1} />,
+  <Zap size={120} color="#fd7902" strokeWidth={1} />,
+  <ClipboardCheck size={120} color="#fd7902" strokeWidth={1} />,
+  <Wrench size={120} color="#fd7902" strokeWidth={1} />,
+  <Headset size={120} color="#fd7902" strokeWidth={1} />,
 ];

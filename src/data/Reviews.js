51987e4680dfba22511f69bbb56e600aa3b9be24
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2026-01-26 15:19:38 +0200

    chrono : test img

--- src/data/Reviews.js
+++ src/data/Reviews.js
@@ -1,5 +1,7 @@
+import img1 from '../../public/images/reviews/review1.jpg';
+
 export const reviews = [
-  { id: 1, rating: 5, image: '*' },
+  { id: 1, rating: 5, image: img1 },
   { id: 2, rating: 5, image: '*' },
   { id: 3, rating: 5, image: '*' },
   { id: 4, rating: 5, image: '*' },

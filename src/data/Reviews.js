89279fd1e2851832fe2d4b905b8ad8b0bb127f03
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2026-02-04 09:16:02 +0200

    chrono : fix rev

--- src/data/Reviews.js
+++ src/data/Reviews.js
@@ -1,10 +1,19 @@
-import img1 from '../../public/images/reviews/review1.jpg';
+// import img1 from '../../public/images/reviews/black-nissan-usa.webp';
+import img2 from '../../public/images/reviews/black-nissan-usa.webp';
+import img3 from '../../public/images/reviews/black-infiniti-usa.webp';
+import img4 from '../../public/images/reviews/white-jeep-usa.webp';
+import img5 from '../../public/images/reviews/blue-tesla-usa.webp';
+import img6 from '../../public/images/reviews/white-tesla-usa.webp';
+
+
+
+
 
 export const reviews = [
-  { id: 1, rating: 5, image: img1 },
-  { id: 2, rating: 5, image: '*' },
-  { id: 3, rating: 5, image: '*' },
-  { id: 4, rating: 5, image: '*' },
-  { id: 5, rating: 5, image: '*' },
-  { id: 6, rating: 5, image: '*' },
+  { id: 1, rating: 5, image: '*' },
+  { id: 2, rating: 5, image: img2 },
+  { id: 3, rating: 5, image: img3 },
+  { id: 4, rating: 5, image: img4 },
+  { id: 5, rating: 5, image: img5 },
+  { id: 6, rating: 5, image: img6 },
 ];

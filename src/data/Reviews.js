6df21648628cfb6c39fc2c0e8562dc539fc8e1be
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2026-02-04 09:35:48 +0200

    chorno: test

--- src/data/Reviews.js
+++ src/data/Reviews.js
@@ -1,16 +1,12 @@
-// import img1 from '../../public/images/reviews/black-nissan-usa.webp';
+import img1 from '../../public/images/reviews/blue-hyundai-usa.webp';
 import img2 from '../../public/images/reviews/black-nissan-usa.webp';
 import img3 from '../../public/images/reviews/black-infiniti-usa.webp';
 import img4 from '../../public/images/reviews/white-jeep-usa.webp';
 import img5 from '../../public/images/reviews/blue-tesla-usa.webp';
 import img6 from '../../public/images/reviews/white-tesla-usa.webp';
 
-
-
-
-
 export const reviews = [
-  { id: 1, rating: 5, image: '*' },
+  { id: 1, rating: 5, image: img1 },
   { id: 2, rating: 5, image: img2 },
   { id: 3, rating: 5, image: img3 },
   { id: 4, rating: 5, image: img4 },

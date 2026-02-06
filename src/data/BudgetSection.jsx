da5e83de73025b4c80d8d72b7c4d06535312b2fc
Oleksandr Chekrenov <olekchekrenov@gmail.com>
2026-02-06 14:39:53 +0200

    fix photos

--- src/data/BudgetSection.jsx
+++ src/data/BudgetSection.jsx
@@ -1,22 +1,28 @@
+import img1 from '../../public/images/budget/white-jetta-usa.webp';
+import img2 from '../../public/images/budget/black-ford-usa.webp';
+import img3 from '../../public/images/budget/silver-mazda-usa.webp';
+import img4 from '../../public/images/budget/silver-audi-usa.webp';
+import img5 from '../../public/images/budget/dark-gray-bmw-usa.webp';
+
 export const budgetData = [
   {
     id: 1,
-    image: '../../public/images/budget/VW_Jetta_15.webp',
+    image: img1,
   },
   {
     id: 2,
-    image: '',
+    image: img2,
   },
   {
     id: 3,
-    image: '',
+    image: img3,
   },
   {
     id: 4,
-    image: '',
+    image: img4,
   },
   {
     id: 5,
-    image: '',
+    image: img5,
   },
 ];
